62d6343a3b150db7768b1790f46150fbc259290e
野間亜沙美 <>
2024-08-10 19:18:24 +0900

    8/10　コミット1

--- AtlasManagementSystem-master/app/Http/Controllers/Authenticated/Top/TopsController.php
+++ AtlasManagementSystem-master/app/Http/Controllers/Authenticated/Top/TopsController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Authenticated\Top;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Users\User;
 use Auth;
 
 class TopsController extends Controller
 {
     public function show(){
+
+        $query = User::with('user')->whereIn('role', [1,2,3]);
         return view('authenticated.top.top');
     }
 
