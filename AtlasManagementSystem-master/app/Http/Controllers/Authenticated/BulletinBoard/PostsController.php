b83f81ea0a2706fd04a6c600cb3775fbed76f3d7
野間亜沙美 <>
2024-11-19 22:20:16 +0900

    11/19　コミット1

--- AtlasManagementSystem-master/app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ AtlasManagementSystem-master/app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -32,7 +32,8 @@ class PostsController extends Controller
             ->orWhere('post', 'like', '%'.$request->keyword.'%')->get();
         }else if($request->category_word){
             $sub_category = $request->category_word;
-            $posts = Post::with('user', 'postComments')->get();
+            $posts = Post::with('user', 'postComments')
+            ->where('sub_category','$sub_category')->get();
         }else if($request->like_posts){
             $likes = Auth::user()->likePostId()->get('like_post_id');
             $posts = Post::with('user', 'postComments')
