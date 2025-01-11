7a788f048d24db3a68fe907ed7eade0d8e763577
野間亜沙美 <>
2025-01-11 17:41:03 +0900

    1/11　コミット2

--- AtlasManagementSystem-master/resources/views/authenticated/bulletinboard/posts.blade.php
+++ AtlasManagementSystem-master/resources/views/authenticated/bulletinboard/posts.blade.php
@@ -9,17 +9,22 @@
       <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
       <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
       <div class="post_bottom_area d-flex">
-        <div class="d-flex post_status">
-          <div class="mr-5">
+        <div class="d-flex post_status w-100 justify-content-end">
+          <div class="d-flex align-items-center mr-3">
             <!-- 9/21　追記　コメント数 -->
-            <i class="fa fa-comment"></i><span class="commentCounts{{ $post->id }}">{{ $post_comment->commentCounts($post->id) }}</span>
+            <i class="fa fa-comment"></i>
+            <span class="commentCounts{{ $post->id }} ml-1">{{ $post_comment->commentCounts($post->id) }}</span>
           </div>
-          <div>
+          <div class="d-flex align-items-center">
             <!-- 9/21　追記　いいね数 -->
             @if(Auth::user()->is_Like($post->id))
-            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span></p>
+            <p class="m-0  d-flex align-items-center">
+              <i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i>
+              <span class="like_counts{{ $post->id }}  ml-1">{{ $like->likeCounts($post->id) }}</span></p>
             @else
-            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $like->likeCounts($post->id) }}</span></p>
+            <p class="m-0 d-flex align-items-center">
+              <i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i>
+              <span class="like_counts{{ $post->id }} ml-1">{{ $like->likeCounts($post->id) }}</span></p>
             @endif
           </div>
         </div>
@@ -29,13 +34,21 @@
   </div>
   <div class="other_area border w-25">
     <div class="border m-4">
-      <div class=""><a href="{{ route('post.input') }}"><button type="button" class="btn btn-info">投稿</button></a></div>
-      <div class="">
-        <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
+      <div class=""><a href="{{ route('post.input') }}"><button type="button" class="btn btn-info post_button w-100">投稿</button></a></div>
+      <div class="input-group search">
+        <input type="text" class="form-control" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
         <input type="submit" value="検索" class="btn btn-info" name="category_word" form="postSearchRequest">
       </div>
-      <input type="submit" name="like_posts" class="btn btn-secondary btnx-indigo category_btn" value="いいねした投稿" form="postSearchRequest">
-      <input type="submit" name="my_posts" class="btn btn-secondary btnx-indigo category_btn" value="自分の投稿" form="postSearchRequest">
+      
+      <!-- 横並び -->
+      <div class="row">
+        <div class="col">
+          <input type="submit" name="like_posts" class="btn pink-bg category_btn w-100" value="いいねした投稿" form="postSearchRequest">
+        </div>
+        <div class="col">
+          <input type="submit" name="my_posts" class="btn yellow-bg category_btn w-100" value="自分の投稿" form="postSearchRequest">
+        </div>
+      </div>
       <ul>
         @foreach($categories as $category)
         <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}<span></li>
@@ -45,7 +58,7 @@
 
     <!-- カテゴリー検索 -->
     <div>
-      <p>カテゴリー検索</p>
+      <p class="category_search_box">カテゴリー検索</p>
       <nav class="category-menu">
         <div class="category-menu-item">
           @foreach($categories as $category) 
